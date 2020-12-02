4cc2f687add97699bb9886a5dbcc76605e457f34
David Margolin <davidmargolin0305@gmail.com>
2020-12-02 16:57:01 +0000

    clean up day 2 solution

--- 2/index.js
+++ 2/index.js
@@ -6,6 +6,7 @@ const text = fs.readFileSync("./2/input.txt", { encoding: "utf-8" });
 const textByLine = text.split("\n");
 
 // Part 1
+
 const entries = textByLine.map(entry => {
     const splitted = entry.split(": ");
     const char = splitted[0].slice(-1);
@@ -38,7 +39,7 @@ console.log("Part 2:");
 
 const totalValid2 = entries.reduce((total, curr) => {
     const {password, min, max , char} = curr;
-    if (((password[min-1] === char) && (password[max-1] != char)) || ((password[max-1] === char) && (password[min-1] != char))){
+    if (password[min-1] === char ^ password[max-1] === char){
         return total + 1;
     }
     return total;
