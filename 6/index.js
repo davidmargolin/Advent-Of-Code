9be2d7a28bce13403f8f2e9f1816e7e3e02f863b
David Margolin <davidmargolin0305@gmail.com>
2020-12-06 05:25:28 +0000

    day 6 answers

---
+++ 6/index.js
@@ -0,0 +1,35 @@
+import fs from "fs";
+
+// parse input
+
+const text = fs.readFileSync("./6/input.txt", { encoding: "utf-8" });
+const textByGroup = text.split("\n\n");
+
+// Part 1
+
+console.log("Part 1:");
+
+const totalAnswers = textByGroup.reduce((total, curr) => {
+    const letters = new Set(curr.split("\n").join("").split(""));
+    return total + letters.size;
+}, 0)
+
+console.log(totalAnswers);
+
+// Part 2
+
+console.log("Part 2:");
+
+const totalOverlappingAnswers = textByGroup.reduce((total, curr) => {
+    const [overlapping, ...otherAnswers] = curr.split("\n").map(line => new Set(line.split("")));
+    otherAnswers.forEach(answers => {
+        overlapping.forEach(overlap => {
+            if (!answers.has(overlap)) {
+                overlapping.delete(overlap);
+            }
+        })
+    })
+    return total + overlapping.size;
+}, 0)
+
+console.log(totalOverlappingAnswers);
