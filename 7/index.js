cb9d5d33613b7324a7cb1140ad6e82706049d2b9
David Margolin <davidmargolin0305@gmail.com>
2020-12-07 06:03:49 +0000

    clean up day 7

--- 7/index.js
+++ 7/index.js
@@ -9,40 +9,33 @@ const textByLine = text.split("\n");
 
 console.log("Part 1:");
 
-const parsedRules = textByLine.map(rule => {
+const containers = textByLine.reduce((bags, rule) => {
     const [bagColor, contains] = rule.split(" bags contain ");
     if (contains === "no other bags.") {
-        return { bagColor, bagsContained: {} };
-    }
-    return {
-        bagColor,
-        bagsContained: contains.replace(".", "").split(", ").reduce((total, curr) => {
+        bags[bagColor] = {};
+    }else{
+        bags[bagColor] = contains.replace(".", "").split(", ").reduce((total, curr) => {
             const colors = curr.replace(" bags", "").replace(" bag", "");
             total[colors.substr(2)] = parseInt(colors.substr(0, 1));
             return total;
-        }, {})
-    };
-})
-
-const containers = parsedRules.reduce((total, curr) => {
-    const { bagColor, bagsContained } = curr;
-    total[bagColor] = bagsContained;
-    return total;
-}, {})
+        }, {});
+    }
+    return bags;
+}, {});
 
 const containsShinyGold = (color) => {
-    const containedColors = Object.keys(containers[color])
+    const containedColors = Object.keys(containers[color]);
     if (containedColors.length === 0) return false;
     if (containedColors.includes("shiny gold")) return true;
     return containedColors.some(containsShinyGold);
 }
 
-const countShinyGold = [...new Set(Object.keys(containers))].reduce((total, curr) => {
+const countShinyGold = Object.keys(containers).reduce((total, curr) => {
     if (containsShinyGold(curr)) {
         total += 1;
     }
     return total;
-}, 0)
+}, 0);
 
 console.log(countShinyGold);
 
@@ -51,7 +44,8 @@ console.log(countShinyGold);
 console.log("Part 2:");
 
 const howManyBags = (color) => Object.keys(containers[color]).reduce((total, curr) => {
-    return total += containers[color][curr] + containers[color][curr] * howManyBags(curr);
+    const currCount = containers[color][curr];
+    return total += currCount + currCount * howManyBags(curr);
 }, 0);
 
 console.log(howManyBags("shiny gold"));
