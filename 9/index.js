eb5860534efb05a96e77040fe8e59c6a1824d34e
David Margolin <davidmargolin0305@gmail.com>
2020-12-09 05:57:53 +0000

    Day 9 solution

---
+++ 9/index.js
@@ -0,0 +1,39 @@
+import fs from "fs";
+
+// parse input
+
+const text = fs.readFileSync("./9/input.txt", { encoding: "utf-8" });
+const textByLine = text.split("\n").map(line => parseInt(line));
+
+// Part 1
+
+console.log("Part 1:");
+
+const notSumOfPrevious = textByLine.slice(25).find((line, index) => {
+    const subList = new Set(textByLine.slice(index, index + 25));
+    return ![...subList].some(number => subList.has(line - number) && line - number != number);
+})
+
+console.log(notSumOfPrevious);
+
+// Part 2
+
+console.log("Part 2:");
+
+let total = 0;
+let leftIndex = 0;
+let rightIndex = 0;
+while (total != notSumOfPrevious) {
+    if (total > notSumOfPrevious) {
+        leftIndex++;
+    } else {
+        rightIndex++;
+    }
+    total = 0;
+    for (let i = leftIndex; i <= rightIndex; i++) {
+        total += textByLine[i];
+    }
+}
+
+const winningSlice = textByLine.slice(leftIndex, rightIndex);
+console.log(Math.max(...winningSlice) + Math.min(...winningSlice));
