afce9ce15277b1def1a140c5eeeceb55dc9473fd
David Margolin <davidmargolin0305@gmail.com>
2021-12-01 05:42:05 +0000

    Day 1 solution

---
+++ 1/index.js
@@ -0,0 +1,35 @@
+import fs from "fs";
+
+// parse input
+
+const text = fs.readFileSync("./1/input.txt", { encoding: "utf-8" });
+const textByLine = text.split("\n");
+
+// Part 1
+
+const entries = textByLine.map(entry => parseInt(entry));
+
+console.log("Part 1:");
+
+function countIncrease(nums) {
+    let count = 0;
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i + 1] > nums[i]) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countIncrease(entries));
+
+// Part 2
+
+console.log("Part 2:");
+
+const trioSums = [];
+for (let i = 0; i < entries.length - 2; i++) {
+    trioSums.push(entries[i] + entries[i + 1] + entries[i + 2]);
+}
+
+console.log(countIncrease(trioSums));
