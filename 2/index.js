4280525694ab54d6556739813a6a0d4240985874
David Margolin <davidmargolin0305@gmail.com>
2022-12-02 20:46:05 +0000

    Day 2 solution.

--- 2/index.js
+++ 2/index.js
@@ -10,6 +10,64 @@ const textByLine = text.split("\n");
 
 console.log("Part 1:");
 
+const playMap = {
+    "X": "A",
+    "Y": "B",
+    "Z": "C",
+};
+
+const plays = textByLine.map(text => {
+    const [opponent, me] = text.split(" ");
+    return { opponent, me: playMap[me] };
+});
+
+const winMap = {
+    "A": "B",
+    "B": "C",
+    "C": "A",
+};
+
+const pointsMap = {
+    "A": 1,
+    "B": 2,
+    "C": 3,
+};
+
+function calcScore(plays) {
+    return plays.reduce((prev, { opponent, me }) => {
+        if (opponent === me) {
+            return prev + pointsMap[me] + 3;
+        }
+        if (winMap[opponent] === me) {
+            return prev + pointsMap[me] + 6;
+        }
+        return prev + pointsMap[me];
+    }, 0);
+}
+
+console.log(calcScore(plays));
+
 // Part 2
 
 console.log("Part 2:");
+
+const loseMap = {
+    "B": "A",
+    "C": "B",
+    "A": "C",
+};
+
+const playsWithExpectation = textByLine.map(text => {
+    const [opponent, expectation] = text.split(" ");
+    if (expectation === "X") {
+        return { opponent, me: loseMap[opponent] };
+    }
+    if (expectation === "Y") {
+        return { opponent, me: opponent };
+    }
+    if (expectation === "Z") {
+        return { opponent, me: winMap[opponent] };
+    }
+});
+
+console.log(calcScore(playsWithExpectation));
