59ad1afc07ec82daa046fed3c2de4e1ec78e0c1e
David Margolin <davidmargolin0305@gmail.com>
2022-12-04 15:56:06 +0000

    Day 4 solution

--- 4/index.js
+++ 4/index.js
@@ -10,9 +10,52 @@ const textByLine = text.split("\n");
 
 console.log("Part 1:");
 
+const sections = textByLine.map(line => {
+    const [a, b] = line.split(",").map(section => {
+        const [start, end] = section.split("-").map(Number);
+        return { start, end };
+    })
+    return { a, b };
+});
 
+function sectionsAreOverlapping(A, B) {
+    if (A.start <= B.start && A.end >= B.end) {
+        return true;
+    }
+    if (B.start <= A.start && B.end >= A.end) {
+        return true;
+    }
+    return false;
+}
+
+const overlapCount = sections.reduce((prev, curr) => {
+    if (sectionsAreOverlapping(curr.a, curr.b)) {
+        return prev += 1;
+    }
+    return prev;
+}, 0);
+
+console.log(overlapCount);
 
 // Part 2
 
 console.log("Part 2:");
 
+function sectionsAreIntersecting(A, B) {
+    if (A.start >= B.start && A.start <= B.end) {
+        return true;
+    }
+    if (B.start >= A.start && B.start <= A.end) {
+        return true;
+    }
+    return false;
+}
+
+const intersectCount = sections.reduce((prev, curr) => {
+    if (sectionsAreIntersecting(curr.a, curr.b)) {
+        return prev += 1;
+    }
+    return prev;
+}, 0);
+
+console.log(intersectCount);
