74f916dddecab3c9876d9f99d48746c70c3f0a87
David Margolin <davidmargolin0305@gmail.com>
2022-12-04 15:56:37 +0000

    setup day 5

---
+++ 5/index.js
@@ -0,0 +1,18 @@
+import fs from "fs";
+import path from "path";
+
+// parse input
+
+const text = fs.readFileSync(path.resolve(__dirname, './input.txt'), { encoding: "utf-8" });
+const textByLine = text.split("\n");
+
+// Part 1
+
+console.log("Part 1:");
+
+
+
+// Part 2
+
+console.log("Part 2:");
+
